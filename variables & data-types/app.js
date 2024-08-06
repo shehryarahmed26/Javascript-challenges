91cfa0b9be685091afb2212fec3027a5e55a7a1e
Shehryar <shehryarahmed268@gmail.com>
2024-08-06 22:57:49 +0500

    'day-1-complete'

---
+++ variables & data-types/app.js
@@ -0,0 +1,48 @@
+// **************************** Day 1 *************************************
+
+// Task#1 --> Declare a variable using var, assign it a number, and log the
+// value to the console .
+
+var num = 10;
+console.log(num);
+
+// Task#2 --> Declare a variable using let, assign it a string, and log the
+// value to the console .
+
+let name = 'Shehryar'
+console.log(name);
+
+// Task#3 --> Declare a variable using const, assign it a boolean, and log the
+// value to the console .
+
+const challenges_completed = false
+console.log(challenges_completed);
+
+// Task#4 --> Declare variables of different data types (number, string, object,
+// array), and log each variable's type using typeof.
+
+let age = 20;
+console.log(`Value Before ressign ${age}`);
+
+let user = 'Ahmed'
+let profile = {
+    email: 'shehryarahmed268@gmail.com',
+    password: 'password'
+}
+let fav_colors = ['black', 'white', 'dark blue']
+
+console.log(typeof age);
+console.log(typeof user);
+console.log(typeof profile);
+console.log(typeof fav_colors);
+
+// Task#5 --> Declare a variables using let, assign it an intial value, reassign,
+// a new value and log both value to the console.
+age = 30
+console.log(`Value After ressign ${age}`);
+
+
+
+
+
+
