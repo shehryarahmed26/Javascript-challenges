fd2ed691064d2257ffd746a96bdaeb3ede0cc8a6
Shehryar <shehryarahmed268@gmail.com>
2024-08-07 00:25:40 +0500

    'day-2-complete'

---
+++ Operators/app.js
@@ -0,0 +1,91 @@
+//  ******************************* Day 2 **********************************
+
+// Arithmethic Operators -->
+
+// Task#1 --> Write a program to add two numbers and log the result to the
+// console.
+
+let num1 = 30;
+let num2 = 20;
+let add = num1 + num2;
+console.log(add);
+
+// Task#1 --> Write a program to substract two numbers and log the result to the
+// console.
+
+let substract = num1 - num2
+console.log(substract);
+
+// Task#1 --> Write a program to multiply two numbers and log the result to the
+// console.
+
+let multiply = num1 * num2
+console.log(multiply);
+
+// Task#1 --> Write a program to divide two numbers and log the result to the
+// console.
+
+let divide = num1 / num2
+console.log(divide);
+
+// Task#1 --> Write a program to find the reminder when one number is divided
+// by another and log the result to the console.
+
+let modulus = num1 % num2
+console.log(modulus);
+
+// Assignment Operators -->
+
+// use the += operator to add a number to a variable and log the result to the
+// console.
+
+num1 += 15;
+console.log(num1);
+
+// use the += operator to add a number to a variable and log the result to the
+// console.
+
+num2 -= 10;
+console.log(num2);
+
+// Comparision Operators -->
+
+// Write a program to compare to numbers using > and < and log the result to the
+// console.
+
+let num3 = '45'
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 == num3);
+console.log(num1 === num2);
+
+// Logical Operators -->
+
+// Write a program that uses the &&, ||, !  operators to combine two conditions and log 
+// the result to the console.
+
+let num4 = 17;
+
+if (num3 && num2 < num1) {
+    console.log('Lets go');
+    
+}
+if (num1 || num4 > num2) {
+    console.log(true);
+    
+}
+console.log(!num4);
+
+// Write a program that uses the ternary operator to check if a number is
+// positive or negative and log the result to the console.
+
+let validation = num4 > 18 ? 'you can drive' : 'you cant drive'
+console.log(validation);
+
+
+
+
+
+
